337fc8dc3b9b674acbe45b1a44a4dc3eedb7cc0c
Josh Strohminger <joshstrohminger@users.noreply.github.com>
2019-09-11 22:45:37 -0700

    scrap the existing client and create a new PWA client

--- Source/TurnTracker.Client/README.md
+++ Source/TurnTracker.Client/README.md
@@ -1,6 +1,6 @@
-# TurnTrackerClient
+# TurntrackerClient
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.7.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.9.
 
 ## Development server
 
--- Source/TurnTracker.Client/src/app/app.component.spec.ts
+++ Source/TurnTracker.Client/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TurnTracker-Client'`, () => {
+  it(`should have as title 'turntracker-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('TurnTracker-Client');
+    expect(app.title).toEqual('turntracker-client');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TurnTracker-Client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to turntracker-client!');
   });
 });
