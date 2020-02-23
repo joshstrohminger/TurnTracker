5ca5084b776e3eb18a67be830e7cbeb441ac8cbf
Josh Strohminger <joshstrohminger@users.noreply.github.com>
2020-02-23 02:49:24 -0800

    entity

--- Source/TurnTracker.Client/src/app/auth/webauthn.service.ts
+++ Source/TurnTracker.Client/src/app/auth/webauthn.service.ts
@@ -12,64 +12,77 @@ export class WebauthnService {
   constructor() { }
 
   private createCredentials(user: IUser, challenge: string) {
-    // const options = <PublicKeyCredentialCreationOptions>{
-    //   challenge: Uint8Array.from(challenge, c => c.charCodeAt(0)),
-    //   rp: {
-    //     name: environment.appName,
-    //     id: window.location.hostname,
-    //   },
-    //   user: {
-    //     id: Uint8Array.from(user.id.toString(), c => c.charCodeAt(0)),
-    //     name: user.username,
-    //     displayName: user.displayName,
-    //   },
-    //   pubKeyCredParams: [{ alg: -7, type: 'public-key' }],
-    //   authenticatorSelection: {
-    //     authenticatorAttachment: 'platform',
-    //   },
-    //   timeout: this.timeout,
-    //   attestation: 'direct'
-    // };
+    const options = <PublicKeyCredentialCreationOptions>{
+      challenge: Uint8Array.from(challenge, c => c.charCodeAt(0)),
+      rp: {
+        name: environment.appName,
+        // id: window.location.host,
+      },
+      user: {
+        id: Uint8Array.from(user.id.toString(), c => c.charCodeAt(0)),
+        name: user.username,
+        displayName: user.displayName,
+      },
+      pubKeyCredParams: [{ alg: -7, type: 'public-key' }],
+      authenticatorSelection: {
+        //authenticatorAttachment: 'platform',
+        userVerification: 'required'
+        //userVerification: 'discouraged'
+      },
+      timeout: 60000,
+      attestation: 'direct'
+    };
 
-    // return navigator.credentials.create({
-    //   publicKey: options
-    // });
+    console.log('creating creds', options);
+
+    return navigator.credentials.create({
+      publicKey: options
+    });
   }
 
   private getCredentialsById(credentialId: string, challenge: string) {
-    // const options = <PublicKeyCredentialRequestOptions>{
-    //   challenge: Uint8Array.from(challenge, c => c.charCodeAt(0)),
-    //   allowCredentials: [{
-    //     id: Uint8Array.from(credentialId, c => c.charCodeAt(0)),
-    //     type: 'public-key',
-    //   }],
-    //   timeout: this.timeout,
-    // };
+    console.log('geting creds by id', credentialId);
 
-    // return navigator.credentials.get({
-    //   publicKey: options
-    // });
+    const options = <PublicKeyCredentialRequestOptions>{
+      challenge: Uint8Array.from(challenge, c => c.charCodeAt(0)),
+      allowCredentials: [{
+        id: Uint8Array.from(credentialId, c => c.charCodeAt(0)),
+        type: 'public-key',
+      }],
+      timeout: 60000,
+    };
+
+    return navigator.credentials.get({
+      publicKey: options
+    });
   }
 
   private getCredentials(challenge: string) {
-    // const options = <PublicKeyCredentialRequestOptions>{
-    //   challenge: Uint8Array.from(challenge, c => c.charCodeAt(0)),
-    //   timeout: this.timeout,
-    // };
+    console.log('getting any');
+    const options = <PublicKeyCredentialRequestOptions>{
+      challenge: Uint8Array.from(challenge, c => c.charCodeAt(0)),
+      timeout: this.timeout,
+    };
 
-    // return navigator.credentials.get({
-    //   publicKey: options
-    // });
+    return navigator.credentials.get({
+      publicKey: options
+    });
   }
 
   public async test(user: IUser) {
-    const newCreds = await this.createCredentials(user, 'first');
+    console.log('checking availablility');
+    const available = await PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable();
+    console.log('available', available);
+
+    if (!available) { return; }
+
+    const newCreds = await this.createCredentials(user, new Date().toString());
     console.log('created', newCreds);
 
-    const matchingCred = await this.getCredentialsById('id', 'second');
+    const matchingCred = await this.getCredentialsById(newCreds.id, new Date().toString());
     console.log('matching', matchingCred);
 
-    const anyCred = await this.getCredentials('third');
+    const anyCred = await this.getCredentials(new Date().toString());
     console.log('any', anyCred);
   }
 }
