386204975e268837422541a2b72ef9097934489c
Josh Strohminger <joshstrohminger@users.noreply.github.com>
2020-02-23 02:49:24 -0800

    entity

---
+++ Source/TurnTracker.Data/Entities/DeviceAuthorization.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TurnTracker.Data.Entities
+{
+    public class DeviceAuthorization : Entity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        [Required]
+        public string PublicKey { get; set; }
+        [Required]
+        public string CredentialId { get; set; }
+
+        #region Navigation Properties
+
+        public User User { get; set; }
+
+        #endregion Navigation Properties
+    }
+}
