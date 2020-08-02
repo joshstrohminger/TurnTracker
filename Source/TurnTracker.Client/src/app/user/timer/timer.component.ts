7d62d199b4ddc422b64c75bea6215fd285390da4
Josh Strohminger <joshstrohminger@users.noreply.github.com>
2020-08-02 16:29:29 -0700

    Update due time display, resolves #9
    Show the non-live time remaining or overdue on the activities page instead of the due date.
    Make label, live updates, and the number of time components in the timer configurable.
    Only show up to 2 time components on the activities page.

--- Source/TurnTracker.Client/src/app/user/timer/timer.component.ts
+++ Source/TurnTracker.Client/src/app/user/timer/timer.component.ts
@@ -9,7 +9,44 @@ import { DateTime, Duration } from 'luxon';
 export class TimerComponent implements OnInit, OnDestroy {
 
   @Input() due: DateTime;
+  @Input() appendLabel: boolean;
+  @Input() set enabled(enable: boolean) {
+    if (!this.initialized) {
+      this._enabled = enable;
+    } else if (!this._enabled && enable) {
+      this.startTimer();
+    } else if (this._enabled && !enable) {
+      this.stopTimer();
+    }
+  }
+  @Input() set maxTimeComponents(max: number) {
+    this.yearFormat = this.trimTimeComponents(this.longYearFormat, max);
+    this.monthFormat = this.trimTimeComponents(this.longMonthFormat, max);
+    this.dayFormat = this.trimTimeComponents(this.longDayFormat, max);
+    this.hourFormat = this.trimTimeComponents(this.longHourFormat, max);
+    this.minuteFormat = this.trimTimeComponents(this.longMinuteFormat, max);
+    this.secondFormat = this.trimTimeComponents(this.longSecondFormat, max);
 
+    if (this.initialized && !this.timerHandle) {
+      this.tick();
+    }
+  }
+
+  readonly longYearFormat = `y'y' M'mo' d'd' h'h' m'm' s's'`;
+  yearFormat = this.longYearFormat;
+  readonly longMonthFormat = `M'mo' d'd' h'h' m'm' s's'`;
+  monthFormat = this.longMonthFormat;
+  readonly longDayFormat = `d'd' h'h' m'm' s's'`;
+  dayFormat = this.longDayFormat;
+  readonly longHourFormat = `h'h' m'm' s's'`;
+  hourFormat = this.longHourFormat;
+  readonly longMinuteFormat = `m'm' s's'`;
+  minuteFormat = this.longMinuteFormat;
+  readonly longSecondFormat = `s's'`;
+  secondFormat = this.longSecondFormat;
+
+  _enabled = false;
+  initialized = false;
   isOverdue = false;
   remaining: string;
 
@@ -25,11 +62,36 @@ export class TimerComponent implements OnInit, OnDestroy {
       throw new TypeError(`The 'due' attribute is invalid: ${this.due.invalidExplanation}`);
     }
 
-    this.timerHandle = window.setInterval(() => this.tick(), 1000);
-    this.tick();
+    if (this._enabled) {
+      this.startTimer();
+    } else {
+      this.tick();
+    }
+    this.initialized = true;
   }
 
   ngOnDestroy(): void {
+    this.stopTimer();
+  }
+
+  private trimTimeComponents(format: string, max: number): string {
+    max = Math.floor(max);
+
+    if (max <= 0) {
+      return format;
+    } else {
+      return format.split(' ').slice(0, max).join(' ');
+    }
+  }
+
+  private startTimer() {
+    if (!this.timerHandle) {
+      this.timerHandle = window.setInterval(() => this.tick(), 1000);
+      this.tick();
+    }
+  }
+
+  private stopTimer() {
     if (this.timerHandle) {
       window.clearInterval(this.timerHandle);
       this.timerHandle = undefined;
@@ -46,20 +108,20 @@ export class TimerComponent implements OnInit, OnDestroy {
   private getFormat(diff: Duration): string {
     const d = diff.shiftTo('years', 'months', 'days', 'hours', 'minutes', 'seconds');
     if (d.years) {
-      return d.toFormat(`y'y' M'm' d'd' h'h' m'm' s's'`);
+      return d.toFormat(this.yearFormat);
     }
     if (d.months) {
-      return d.toFormat(`M'm' d'd' h'h' m'm' s's'`);
+      return d.toFormat(this.monthFormat);
     }
     if (d.days) {
-      return d.toFormat(`d'd' h'h' m'm' s's'`);
+      return d.toFormat(this.dayFormat);
     }
     if (d.hours) {
-      return d.toFormat(`h'h' m'm' s's'`);
+      return d.toFormat(this.hourFormat);
     }
     if (d.minutes) {
-      return d.toFormat(`m'm' s's'`);
+      return d.toFormat(this.minuteFormat);
     }
-    return d.toFormat(`s's'`);
+    return d.toFormat(this.secondFormat);
   }
 }
