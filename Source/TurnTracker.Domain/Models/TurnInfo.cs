cb02b4847019efc780043986146582241416fb83
Josh Strohminger <joshstrohminger@users.noreply.github.com>
2021-01-23 01:21:36 -0800

    Update Turn ID after it's been saved. Fixes #15.
    Hold onto the Turn instance if the ID hasn't been set and update the copy of it once it has been set.

--- Source/TurnTracker.Domain/Models/TurnInfo.cs
+++ Source/TurnTracker.Domain/Models/TurnInfo.cs
@@ -5,7 +5,23 @@ namespace TurnTracker.Domain.Models
 {
     public class TurnInfo
     {
-        public int Id { get; }
+        private Turn _turn;
+        private int _id;
+
+        public int Id
+        {
+            get
+            {
+                if (_id == 0 && _turn != null && _turn.Id != 0)
+                {
+                    // The turn has been saved and an updated ID is available
+                    _id = _turn.Id;
+                    _turn = null;
+                }
+
+                return _id;
+            }
+        }
         public int UserId { get; }
         public DateTimeOffset Occurred { get; }
         public DateTimeOffset Created { get; }
@@ -16,7 +32,12 @@ namespace TurnTracker.Domain.Models
 
         public TurnInfo(Turn turn)
         {
-            Id = turn.Id;
+            _id = turn.Id;
+            if (_id == 0)
+            {
+                // keep track of the turn so it can be used to retrieve an updated id once it's been saved
+                _turn = turn;
+            }
             UserId = turn.UserId;
             Occurred = turn.Occurred;
             Created = turn.CreatedDate;
